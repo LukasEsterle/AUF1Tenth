55a23fc381e0329505f8c48fb2f7539404df2eb3
MaltheT <malthe@toettrup.dk>
2022-05-24 10:15:24 +0200

    Comments and update for lidar data processor
    change the code to be more dynamical, hopefully this will not introduce issues

--- digital_twin/interface_physical_twin/ros/lidar_data_processer.py
+++ digital_twin/interface_physical_twin/ros/lidar_data_processer.py
@@ -1,122 +1,152 @@
 #!/usr/bin/env python
-#for ros
-from requests import request
-import rospy
-
-#for rappidmq
-import pika
+# for ros
 import datetime
 import json
 
 import numpy as np
+# for rappidmq
+import pika
+import rospy
+from requests import request
+from torch import half
 
-class LidarDataProcesser():
+
+class LidarDataProcesser:
     def __init__(self):
-        
-        #rospy
-        rospy.init_node('LidarDataProcesser', anonymous=True)
-        self.rate = rospy.Rate(100) # 10hz
-        
-        #rabbitmq
-        
-        self.credentials = pika.PlainCredentials('guest', 'guest')
-        self.connectionToFMU = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self.connectionFromRobot = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.1.3', port=5672, credentials=self.credentials))
+
+        # rospy
+        rospy.init_node("LidarDataProcesser", anonymous=True)
+        self.rate = rospy.Rate(100)  # 100hz
+
+        # rabbitmq
+
+        self.credentials = pika.PlainCredentials("guest", "guest")
+        self.connectionToFMU = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
+        self.connectionFromRobot = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host="192.168.1.3", port=5672, credentials=self.credentials
+            )
+        )
         """
         self.connectionToFMU = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.connectionFromRobot = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         """
-        #channel to the FMU
+        # channel to the FMU
         self.channelToFmu = self.connectionToFMU.channel()
-        self.channelToFmu.exchange_declare(exchange='topic_logs', exchange_type='topic')
-            
-        #channel from the robot
+        self.channelToFmu.exchange_declare(
+            exchange="topic_logs", exchange_type="topic")
+
+        # channel from the robot
         self.channelRobotLidar = self.connectionFromRobot.channel()
-            
-        self.channelRobotLidar.exchange_declare(exchange='topic_logs', exchange_type='topic')     
-            
-        result = self.channelRobotLidar.queue_declare('RobotToRos', exclusive=True)
+
+        self.channelRobotLidar.exchange_declare(
+            exchange="topic_logs", exchange_type="topic"
+        )
+
+        result = self.channelRobotLidar.queue_declare(
+            "RobotToRos", exclusive=True)
         queue_name = result.method.queue
 
-
         self.channelRobotLidar.queue_bind(
-            exchange = 'topic_logs', queue=queue_name, routing_key="lidar.ranges")
+            exchange="topic_logs", queue=queue_name, routing_key="lidar.ranges"
+        )
 
-        
         self.channelRobotLidar.basic_consume(
-            queue=queue_name, on_message_callback=self.lidar_callback, auto_ack=True)
-        
+            queue=queue_name, on_message_callback=self.lidar_callback, auto_ack=True
+        )
+
         print("spinning")
         self.channelRobotLidar.start_consuming()
+
+    """ 
+    Function takes ranges from a lidar scan, and uses averaging to find
+    the angle which is the furthest away based on the average,
+    and the corresponding distance.
+    """
+    def lidar_data_averageing(self, body, points_for_average=50, number_of_scans=540):
         
+        """Half the number of scans."""
+        half_nos = int(number_of_scans / 2)
 
-    # Function takes ranges from a lidar scan, and uses averaging to find 
-    # the angle which is the furthest away based on the average, 
-    # and the corresponding distance
-    def lidar_data_averageing(self, ranges, points_for_average=50):
-
-            number_of_scans = 540
-
-            # distance between each scan in radians
-            phi = np.pi/number_of_scans 
-
-            average_ranges = np.zeros(540)
-            half = int(points_for_average/2)
-
-            for i in range(half):
-                ranges[i] = ranges[half]
-                ranges[i + 565] = ranges[565]
-            
-            for i in range(540):
-                average_ranges[i] = np.average(ranges[i: i + points_for_average])
-                
-            # find the longest distance and corresponding angle
-            idx = np.argmax(average_ranges)
-            distance = average_ranges[idx]
-            angle = phi*idx - np.pi/2
-
-            return distance, angle
-
-    def lidar_callback(self, ch, method, properties, body):  
         
-        #get time with for rabbit msg
+        """Distance between each scan in radians."""
+        phi = (np.pi / number_of_scans) # 3/4 pi because the lidar only covers 270 degrees
+
+
+        """ Half the number of points for average."""
+        half_pfa = int(points_for_average / 2)
+
+        """ 
+        Only using scans in the filed of view at the front of the car
+        (so that we don't try to drive to somwhere behind us) 
+        """
+        ranges = body["ranges"][half_nos - half_pfa: number_of_scans + half_nos + half_pfa]
+
+
+        """ 
+        Set points distance of the points at the left and right side of the field of view,
+        equal to the left and right scan distance respectively. 
+        """
+        average_ranges = np.zeros(number_of_scans)
+        for i in range(half_pfa):
+            ranges[i] = ranges[half_pfa]
+            ranges[i + number_of_scans +
+                   half_pfa] = ranges[number_of_scans + half_pfa]
+
+        """Compute the average around each point for N neighbours."""
+        for i in range(number_of_scans):
+            average_ranges[i] = np.average(ranges[i: i + points_for_average])
+
+        """ Find the point with longest average distance and corresponding angle. """
+        idx = np.argmax(average_ranges)
+        distance = average_ranges[idx]
+        angle = phi * idx - np.pi / 2
+
+        return distance, angle
+
+    def lidar_callback(self, ch, method, properties, body):
+
+        # get time with for rabbit msg
         rt = rospy.get_rostime()
         rostime = rt.secs + rt.nsecs * 1e-09
-        rostimeISO = datetime.datetime.strptime(datetime.datetime.utcfromtimestamp(rostime).isoformat(timespec='milliseconds')+'+0100', "%Y-%m-%dT%H:%M:%S.%f%z")
-        
-        #checking if the send channel and connection is open
-        if not self.channelToFmu or self.connectionToFMU.is_closed:
-            self.connectionToFMU = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-            self.channelToFmu = self.connectionToFMU.channel()
-            self.channelToFmu.exchange_declare(exchange='topic_logs', exchange_type='topic')
-            
-        body = json.loads(body) 
-        
-        # Only using scans in the filed of view at the front of the car 
-        # (so that we don't try to drive to somwhere behind us)
-        ranges = body['ranges'][245: 835]
+        rostimeISO = datetime.datetime.strptime(
+            datetime.datetime.utcfromtimestamp(rostime).isoformat(
+                timespec="milliseconds"
+            )
+            + "+0100",
+            "%Y-%m-%dT%H:%M:%S.%f%z",
+        )
 
-        distance, angle = self.lidar_data_averageing(ranges)
-                 
-        print(len(body['ranges']))
-        
+        # checking if the send channel and connection is open
+        if not self.channelToFmu or self.connectionToFMU.is_closed:
+            self.connectionToFMU = pika.BlockingConnection(
+                pika.ConnectionParameters(host="localhost")
+            )
+            self.channelToFmu = self.connectionToFMU.channel()
+            self.channelToFmu.exchange_declare(
+                exchange="topic_logs", exchange_type="topic"
+            )
+
+        body = json.loads(body)
+        distance, angle = self.lidar_data_averageing(body, points_for_average=150)
+
         routing_key = "fmu.input.targets"
         message = {
-            'time': rostimeISO.isoformat(timespec='milliseconds'),         
-            'distance': distance,
-            'angle': angle
-            }
-            
+            "time": rostimeISO.isoformat(timespec="milliseconds"),
+            "distance": distance,
+            "angle": angle,
+        }
+
         self.channelToFmu.basic_publish(
-            exchange='topic_logs', routing_key=routing_key, body=json.dumps(message))
-        
-        
-if __name__ == '__main__': 
-    try: 
+            exchange="topic_logs", routing_key=routing_key, body=json.dumps(message)
+        )
+
+
+if __name__ == "__main__":
+    try:
         LidarDataProcesser()
 
     except rospy.ROSInterruptException:
         pass
-
-    
-
